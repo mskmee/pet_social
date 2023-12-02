9ba834fccd01622860aae981b7ec2efc6f35cb78
Maksim Maksimenko <85788646+mskmee@users.noreply.github.com>
2023-12-02 18:35:05 +0200

    add: backend

---
+++ api/index.js
@@ -0,0 +1,121 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const {PORT, URL, EMAIL_PASSWORD, MONGO_PASSWORD} = process.env;
+
+const app = express();
+const cors = require('cors');
+app.use(cors());
+
+const fullUrl = `${URL}:${PORT}`;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+const User = require('./models/user');
+const Post = require('./models/post');
+
+const generateSecretKey = () => {
+  return crypto.randomBytes(32).toString('hex');
+};
+
+const secretKey = generateSecretKey();
+
+const sendVerificationEmail = async (email, verificationToken) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'mskmee2@gmail.com',
+      pass: EMAIL_PASSWORD,
+    },
+  });
+  const mailOptions = {
+    from: 'app@gmail.com',
+    to: email,
+    subject: 'Verification',
+    text: `Please click the following link verification: ${URL}:${PORT}/verify/${verificationToken}`,
+  };
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log('Verification email sent');
+  } catch (error) {
+    console.log('Error sending verification email', error);
+  }
+};
+
+mongoose
+  .connect(
+    `mongodb+srv://admin:${MONGO_PASSWORD}@cluster0.okahdbs.mongodb.net/`,
+  )
+  .then(() => {
+    console.log('connected to Mongodb');
+  })
+  .catch(err => {
+    console.log(`Connect to mongodb failed: ${err}`);
+  });
+
+app.post('/register', async (req, res) => {
+  try {
+    const {name, email, password, profileImage} = req.body;
+    if (!name || !email || !password) {
+      throw new Error('Entry required fields');
+    }
+    const isEmailExist = await User.findOne({email: email});
+    if (isEmailExist) {
+      return res.status(400).json({message: 'Email already exists'});
+    }
+    const newUser = new User({name, email, password, profileImage});
+    newUser.verificationToken = crypto.randomBytes(20).toString('hex');
+    await newUser.save();
+    sendVerificationEmail(newUser.email, newUser.verificationToken);
+    res.status(200).json({
+      message: 'Registration successful. Verification email sent successfully',
+    });
+  } catch (error) {
+    console.log('Error registering', error);
+    res.status(500).json({message: `Registration failed. ${error.message}`});
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    const {email, password} = req.body;
+    const user = await User.findOne({email});
+    if (!user) {
+      return res.status(401).json({message: 'Invalid username or password'});
+    }
+    if (user.password !== password) {
+      return res.status(401).json({message: 'Invalid username or password'});
+    }
+    const token = jwt.sign({userId: user._id}, secretKey);
+    res.status(200).json({token});
+  } catch (error) {
+    console.log('Error registering', error);
+    res.status(500).json({message: 'Login failed'});
+  }
+});
+
+app.get('/verify/:verificationToken', async (req, res) => {
+  try {
+    const {verificationToken} = req.params;
+    const user = await User.findOne({verificationToken});
+    if (!user) {
+      console.log('Error verifying didnt find user');
+      return res.status(404).json({message: 'Verification token not found'});
+    }
+    user.verified = true;
+    user.verificationToken = undefined;
+    await user.save();
+    return res.status(200).json({message: 'Verification successful'});
+  } catch (error) {
+    console.log('Error verifying', error);
+    res.status(500).json({message: 'Verify token failed'});
+  }
+});
+
+app.listen(PORT, () => console.log('Server running on port', fullUrl));
---
+++ api/models/user.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  verified: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: String,
+  profileImage: String,
+  userDescription: {
+    type: String,
+    default: null,
+  },
+  connections: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  connectionRequests: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  sentConnectionRequests: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Post',
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
